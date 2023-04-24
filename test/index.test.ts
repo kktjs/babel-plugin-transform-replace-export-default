7613caad3b62fd8df24e4d1ed3ec7f179d297ec3
jaywcjlove <398188662@qq.com>
2023-04-24 16:56:12 +0800

    chore(deps): update dependency tsbb to v4 #3

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,7 +9,7 @@ const defaultBabelOptions = {
 test(`Case const`, () => {
   const babelOptions = { ...defaultBabelOptions };
   const source = `const demo = 0;\n export default demo;`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `const demo = 0;\nreturn demo;`;
   expect(code).toBe(expected);
 });
@@ -17,23 +17,23 @@ test(`Case const`, () => {
 test(`Case function`, () => {
   const babelOptions = { ...defaultBabelOptions };
   const source = `function demo() {};export default demo;`;
-  const { code } = transformSync(source, babelOptions);
-  const expected = `function demo() {}\n\n;\nreturn demo;`;
+  const { code } = transformSync(source, babelOptions) || {};
+  const expected = `function demo() {}\n;\nreturn demo;`;
   expect(code).toBe(expected);
 });
 
 test(`Case class`, () => {
   const babelOptions = { ...defaultBabelOptions };
   const source = `class demo { constructor() { } };export default demo;`;
-  const { code } = transformSync(source, babelOptions);
-  const expected = `class demo {\n  constructor() {}\n\n}\n\n;\nreturn demo;`;
+  const { code } = transformSync(source, babelOptions) || {};
+  const expected = `class demo {\n  constructor() {}\n}\n;\nreturn demo;`;
   expect(code).toBe(expected);
 });
 
 test(`Case 02`, () => {
   const babelOptions = { ...defaultBabelOptions };
   const source = `export default function demo() {}`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `return function demo() {};`;
   expect(code).toBe(expected);
 });
@@ -41,7 +41,7 @@ test(`Case 02`, () => {
 test(`Case 03`, () => {
   const babelOptions = { ...defaultBabelOptions };
   const source = `export default class demo { }`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `return class demo {};`;
   expect(code).toBe(expected);
 });
@@ -53,7 +53,7 @@ test(`Case options remove export default 01`, () => {
     }]]
   };
   const source = `export default class demo { }`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `class demo {}`;
   expect(code).toBe(expected);
 });
@@ -65,7 +65,7 @@ test(`Case options remove export default 02`, () => {
     ]
   };
   const source = `export default function demo() {}`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `function demo() {}`;
   expect(code).toBe(expected);
 });
@@ -78,7 +78,7 @@ test(`Case options remove export default 03`, () => {
     ]
   };
   const source = `class demo {}\nexport default demo;`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `class demo {}`;
   expect(code).toBe(expected);
 });
@@ -90,7 +90,7 @@ test(`Case options remove export default 04`, () => {
     ]
   };
   const source = `function demo() {}\nexport default demo;`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `function demo() {}`;
   expect(code).toBe(expected);
 });
@@ -102,7 +102,7 @@ test(`Case options remove export default 05`, () => {
     ]
   };
   const source = `const demo = 0;\n export default demo;`;
-  const { code } = transformSync(source, babelOptions);
+  const { code } = transformSync(source, babelOptions) || {};
   const expected = `const demo = 0;`;
   expect(code).toBe(expected);
 });
